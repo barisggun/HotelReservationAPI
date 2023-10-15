aeb331e56b94d3702e321940f811e459c0ed002d
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-10-15 11:02:57 +0300

    Add room crud operations with api

--- ApiConsume/HotelProject.WebApiConsume/Program.cs
+++ ApiConsume/HotelProject.WebApiConsume/Program.cs
@@ -1,3 +1,9 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//my codes start
+builder.Services.AddDbContext<Context>();
+builder.Services.AddScoped<IStaffDal,EfStaffDal>(); //Hafýzada 1 kez nesne örneði oluþtur ve bunu kullan.
+builder.Services.AddScoped<IStaffService,StaffManager>();
+//end
+
+
 
 var app = builder.Build();
 
