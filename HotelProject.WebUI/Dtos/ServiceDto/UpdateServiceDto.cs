31bcb736f4ebc7abae56246641bba8a9b5b0c7b9
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-10-19 17:22:19 +0300

    Add UI Dto and ServiceDto

---
+++ HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Hizmet ikon linkini giriniz.")]
+        public string? ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet başlığı giriniz.")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı maksimum 100 karakter olabilir.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Açıklama giriniz.")]
+        [StringLength(500, ErrorMessage = "Açıklama maksimum 500 karakter olabilir.")]
+        public string Description { get; set; }
+    }
+}
