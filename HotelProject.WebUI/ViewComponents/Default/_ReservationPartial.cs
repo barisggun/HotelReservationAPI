6be229394e554944f5df5e3dde7e3198c91311a2
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-10-23 23:03:13 +0300

    Add hotel reservation homepage theme and split index to view components

---
+++ HotelProject.WebUI/ViewComponents/Default/_ReservationPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _ReservationPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
