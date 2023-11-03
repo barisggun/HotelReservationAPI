99d9e4941450c3b458d370726d87d03b050ea41d
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-11-03 18:47:45 +0300

    add jwt project

---
+++ WebApiJwt/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//My codes start
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
