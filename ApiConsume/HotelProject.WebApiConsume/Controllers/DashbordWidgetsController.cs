25e0ff26da214c01014be44079482e2b7be8b2d6
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-11-09 15:39:55 +0300

    add staff count api and dashboard view components

---
+++ ApiConsume/HotelProject.WebApiConsume/Controllers/DashbordWidgetsController.cs
@@ -0,0 +1,26 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashbordWidgetsController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+
+        public DashbordWidgetsController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpGet("StaffCount")]
+        public IActionResult StaffCount()
+        {
+            var value = _staffService.TGetStaffCount();
+            return Ok(value);
+        }
+
+    }
+}
