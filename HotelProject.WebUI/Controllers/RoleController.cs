af602b7211652b329fd7e97c7b650b1e72cd9945
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-11-13 15:07:15 +0300

    role işlemleri

---
+++ HotelProject.WebUI/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+
+
+    }
+}
